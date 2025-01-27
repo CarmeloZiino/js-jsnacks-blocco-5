de8525da1f02ed2adf7397da2cd67ad8d19bdc83
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-01-27 12:35:42 +0100

    Quinto Snack, completato

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,9 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+let pari = nums.filter ((nums)=>{
+return nums % 2 === 0;
+})
+
+console.log(`I numeri pari dentro l'array sono: ${pari}`)
