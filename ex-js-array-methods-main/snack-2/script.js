18f1173654514730cceb53de30a1e0f62224fae2
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-01-27 12:24:44 +0100

    Secondo Snack, completato

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -6,3 +6,13 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+let nomeOggetto = '';
+
+people.forEach((element, index) => {
+
+  nomeOggetto += (index === 0 ? '' : ', ') + element.name;
+
+});
+
+console.log(nomeOggetto);
