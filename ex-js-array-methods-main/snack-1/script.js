f380b53e4f5f55fa80bd6d6707ade4a9e9f7fbc8
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-01-27 12:22:17 +0100

    Primo Snack, completato

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -2,3 +2,12 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+let name = '';
+
+names.forEach((element, index) => {
+
+name += (index === 0 ? '' : ', ') + element;
+
+});
+
+console.log(name);
